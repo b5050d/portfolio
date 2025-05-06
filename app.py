1b7cc677b9d014dab44531a1f5516e3dd33946e4
b5050d <bdyer5280@gmail.com>
2025-05-06 06:49:00 -0600

    Improved some formatting and added the about me section

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/projects')
+def projects():
+    # return render_template('projects.html')
+    return "Hello World"
+
 if __name__ == '__main__':
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -6,3 +6,10 @@ def test_home_route():
     response = client.get('/')
     assert response.status_code == 200
     assert b"Welcome to My Portfolio" in response.data
+
+# TODO - Check that the sections load when loading the home page
+
+
+# TODO - Test that pressing the View Resume button actually takes you
+# to the resume file
+
