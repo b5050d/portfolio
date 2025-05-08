78222b9715c3626966738ac38dea2fcb8f5fd983
b5050d <bdyer5280@gmail.com>
2025-05-07 19:38:12 -0600

    added the rest of the work experiences

--- work_experiences.py
+++ work_experiences.py
@@ -36,4 +36,37 @@ work_experiences.append(
     )
 )
 
+work_experiences.append(
+    WorkExperience(
+        position="Test Engineer",
+        company="Blue Canyon Technologies",
+        location="Lafayette, CO",
+        start_date="Nov, 2022",
+        end_date="Aug, 2023",
+        bullets=[
+            "Automated production and administrative tasks using Ruby, Python, and VBA to improve lab efficiency",
+            "Developed utilities in Python and Ruby to automate routine oscilloscope captures and generate HTML reports.",
+            "Used Ball Aerospace's COSMOS (Ruby) to create, modify, and debug automated test scripts for electrical hardware",
+            "Performed functional testing on flight and GSE electrical hardware, ensuring compliance with specifications.",
+            "Created software-driven automation concepts and presented scope-of-work, POCs, and cost estimates to leadership.",
+        ]
+    )
+)
+
+work_experiences.append(
+    WorkExperience(
+        position="Mechanical/Test Engineer",    
+        company="Microsoft",
+        location="Fort Collins, CO",
+        start_date="June, 2021",
+        end_date="Nov, 2022",
+        bullets=[
+            "Developed machine-vision algorithms to automate object detection and camera targeting using motors.",
+            "Automated sensor testing and data processing with Python, MATLAB, and Powershell, improving testing efficiency.",
+            "Designed software applications for communication with testing equipment via NET, I2C, and serial connections.",
+            "Built simple electrical systems to drive stepper motors, LEDs, microcontrollers, and read thermocouples.",
+            "Designed and 3D-printed camera testing equipment to meet project specifications.",
+        ]
+    )
+)
 
