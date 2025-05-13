7249bf64768435dbbbd3d47231d29d486f9f9354
b5050d <bdyer5280@gmail.com>
2025-05-13 06:49:13 -0600

    Updated the project cards

--- projects.py
+++ projects.py
@@ -7,6 +7,7 @@ class Project:
         bullets: list = [],
         bullet_colors: list = [],
         image_path: str = "",
+        video_link: str = None,
     ):
         self.id = id
         self.name = name
@@ -14,6 +15,7 @@ class Project:
         self.bullets = bullets
         self.bullet_colors = bullet_colors
         self.image_path = image_path
+        self.video_link = video_link
 
 
 my_projects = []
@@ -29,6 +31,39 @@ my_projects.append(
             "Programming",
         ],
         bullet_colors=["blue", "green", "yellow"],
-        image_path="moose.png",
+        image_path="simracing_pedals.png",
+        video_link="https://youtu.be/AL3es1MB4ro?si=Uv6rZIEc8pcrCamo",
+    )
+)
+
+my_projects.append(
+    Project(
+        id=1,
+        name="Sim Racing Handbrake",
+        description="I created a3D printed sim-racing handbrake",
+        bullets=[
+            "3D Printing",
+            "Embedded Systems",
+            "Programming",
+        ],
+        bullet_colors=["blue", "green", "yellow"],
+        image_path="simracing_handbrake.png",
+        video_link="https://youtu.be/dmfHwvvCK10?si=B28gp7n3u9sgrLMc",
+    )
+)
+
+my_projects.append(
+    Project(
+        id=2,
+        name="Sim Racing Shifter",
+        description="I created a 3D printed sim-racing shifter",
+        bullets=[
+            "3D Printing",
+            "Embedded Systems",
+            "Programming",
+        ],
+        bullet_colors=["blue", "green", "yellow"],
+        image_path="simracing_shifter.png",
+        video_link="https://youtu.be/JAJKFBZnZKw?si=0lSRrN2TIkIIuYSD",
     )
 )
