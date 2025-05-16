ee950ef65293ca3b0d8f439ff533e9e72f2eae75
b5050d <bdyer5280@gmail.com>
2025-05-16 06:58:03 -0600

    Added basis of cookie listing cards, need to implement functionality though and such

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    request,
+    make_response,
+    render_template_string,
+    # session,
+)
+
+import os
 
 from home_objects import educations
 from projects import my_projects
 from skills import skill_categories
 from work_experiences import work_experiences
+from cookies_objects import cookies
+
+from dotenv import load_dotenv
+
+load_dotenv("/home/b5050d/secrets/myapp.env")
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "dev-default-key")
 
 
 @app.route("/")
@@ -28,6 +43,55 @@ def projects():
 def project_page(project_id):
     project = my_projects[project_id]
     return render_template("project_page.html", project=project)
+
+
+#################################################################
+# On Call wCookies
+#################################################################
+
+
+@app.route("/cookies")
+def cookies_home():
+    print(cookies)
+    return render_template("cookies_home.html", cookies=cookies)
+
+
+@app.route("/cookie/<int:cookie_id>")
+def cookie(cookie_id):
+    cookie = cookies[cookie_id]
+    return render_template("cookie_page.html", cookie=cookie)
+
+
+#################################################################
+# Debugging / Scratch
+#################################################################
+
+"""
+Alright now I need to implment a cart on this guy
+
+so how would a cart work? Certain actions would add something to the cart
+"""
+
+
+@app.route("/test")
+def test():
+    count = int(request.cookies.get("count", 0))
+    html = f"""
+        <h1>Button clicked {count} times</h1>
+        <form method="POST" action="/increment">
+            <button type="submit">Click Me</button>
+        </form>
+    """
+    return render_template_string(html)
+
+
+@app.route("/increment", methods=["POST"])
+def increment():
+    count = int(request.cookies.get("count", 0)) + 1
+    resp = make_response("", 303)
+    resp.headers["Location"] = "/test"
+    resp.set_cookie("count", str(count))
+    return resp
 
 
 if __name__ == "__main__":
---
+++ cookies_objects.py
@@ -0,0 +1,22 @@
+class CookieListing:
+    """
+    Cookie Object
+
+    You can buy these
+    """
+
+    def __init__(self, id, name, description, price, image_path):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.image_path = image_path
+
+
+cookies = [
+    CookieListing(
+        0, "12x Cookies", "Enough for small gatherings", 20, "cookies_12.png"
+    ),
+    CookieListing(1, "24x Cookies", "Can't go wrong with 24", 37.5, "cookies_24.png"),
+    CookieListing(2, "36x Cookies", "Enough for a full party", 50, "cookies_36.png"),
+]
